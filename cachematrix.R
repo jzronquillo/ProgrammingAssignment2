17510372c47b7fccd2af6c12938b896aec9fb792
jzronquillo <jzronquillo@up.edu.ph>
2017-12-03 04:59:39 +0300

    v3
    i->inv, setInverse->setsolve, etc.
    w/o error;

--- cachematrix.R
+++ cachematrix.R
@@ -5,46 +5,65 @@
 ## makeCacheMatrix() creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-    i<-NULL
-    
+    inv<-NULL
     set<-function(y){
         x<<-y
-        i<<-NULL
+        inv<<-NULL
     } 
     get<-function() x
-    
-    #?
-    setInverse<-function(solve) i<<-solve
-    
-    getInverse<-function() i
-    
-    list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+    setsolve<-function(solve) inv<<-solve
+    getsolve<-function() inv
+    list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
 }
-
 
 ## cacheSolve() computes the inverse of the special "matrix" returned by makeCacheMatrix()
 ## above. If the inverse has already been calculated (and the matrix has not changed),
 ## cacheSolve() retrieves the inverse from the cache.
 
 cacheSolve <- function(x,...) {
-        ## Return a matrix that is the inverse of 'x'
-    i<-x$getInverse()
-    if(!is.null(i)){
+    ## Return a matrix that is the inverse of 'x'
+    inv<-x$getsolve()
+    if(!is.null(inv)){
         message("Getting cached data")
-        return(i)
+        return(inv)
     }
     data<-x$get()
-    i<-solve(data,...)
-    x$setInverse(i)
-    i
+    inv<-solve(data)
+    x$setsolve(inv)
+    inv
 }
 
-##TEST
-W<-matrix(c(1,2,2,1),nrow=2,byrow=TRUE)
-makeCacheMatrix(W)
-cacheSolve(W)
 
-Y<-matrix(c(1,3,3,2,3,4,3,2,4),nrow=3, byrow=TRUE)
-makeCacheMatrix(Y)
-cacheSolve(Y)
+## Matrices for testing the R functions 
+
+##t1
+a<-matrix(c(1,2,2,1),nrow=2,byrow=TRUE)
+a1<-makeCacheMatrix(a)
+cacheSolve(a1)
+##         [,1]       [,2]
+## [1,] -0.3333333  0.6666667
+## [2,]  0.6666667 -0.3333333
+
+##t2
+b<-matrix(c(1,3,3,2,3,4,3,2,4),nrow=3, byrow=TRUE)
+b1<-makeCacheMatrix(b)
+cacheSolve(b1)
+##        [,1] [,2] [,3]
+## [1,]    4   -6    3
+## [2,]    4   -5    2
+## [3,]   -5    7   -3
+
+##t3
+c <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+c1<-makeCacheMatrix(c)
+cacheSolve(c1)
+##        [,1] [,2]
+## [1,]    6    8
+## [2,]    2    4
+
+
+
+
+
+
 
